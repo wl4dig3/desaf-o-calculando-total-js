b9fffab3cf485434984fc65a1efe5ac6a4ee2820
wl4dig3 <wladige@gmail.com>
2024-01-11 21:26:59 -0300

    funcion de incremetar

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-precio = 400000
+let precio = 400000
 
-precioSpan = document.querySelector(".precio-inicial");
+let precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+let btnIncrementar = document.querySelector('#incrementar');
+let btnDisminuir = document.querySelector('#disminuir');
+var cantidad = document.querySelector('.cantidad');
+console.dir(cantidad)
+var a = 0;
+
+btnIncrementar =  ()=> {
+    a++
+     a = ( a < 10) ? "0" + a :  a;
+    cantidad.innerHTML = a;
+     console.log('incremento', a);
+};
+

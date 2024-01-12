eebe40cfa965ed15aaaa9b35f8a564e2cf999b4f
wl4dig3 <wladige@gmail.com>
2024-01-11 22:00:10 -0300

    feat: funcion disminuir, y totalizacion

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-let precio = 400000
+const precio = 400000
 
 let precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
 
+let valorTotal = document.querySelector('.valor-total')
+console.log('1er console', valorTotal);
+
 let btnIncrementar = document.querySelector('#incrementar');
 let btnDisminuir = document.querySelector('#disminuir');
 var cantidad = document.querySelector('.cantidad');
-console.dir(cantidad)
-var a = 0;
+var a = 1;
 
 btnIncrementar =  ()=> {
     a++
-     a = ( a < 10) ? "0" + a :  a;
+    // a = ( a < 10) ? "0" + a :  a;
     cantidad.innerHTML = a;
-     console.log('incremento', a);
+    valorTotal.innerHTML = a * precio;
 };
+console.log(typeof btnIncrementar)
+btnDisminuir =  ()=> {
+    if (a >= 1) {
+        a--
+         //a = ( a < 10) ? "0" + a :  a;
+        cantidad.innerHTML = a;
+        valorTotal.innerHTML = a * precio;
+
+    }
+};
+
+// precioSpan.innerHTML = a * precio
+
 
